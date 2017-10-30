7c1fda8d845a30a2799cafe160bdffa929fa4169
Philippe DAMOUNE <pdamoune@e2r4p12.42.fr>
2017-10-30 18:41:34 +0100

    main with cliopts, header and makefile set

--- projects/srcs/main.c
+++ projects/srcs/main.c
@@ -6,15 +6,25 @@
 /*   By: pdamoune <pdamoune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/30 10:40:26 by pdamoune          #+#    #+#             */
-/*   Updated: 2017/10/30 10:48:10 by pdamoune         ###   ########.fr       */
+/*   Updated: 2017/10/30 18:40:39 by pdamoune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <header.h>
 
+static t_cliopts	g_read_opts[] =
+{
+	{'h', "help", (1 << 0), 0, NULL, 0},
+	{0, 0, 0, 0, 0, 0},
+};
+
 int		main(int ac, char **av)
 {
-	(void)ac;
-	(void)av;
+	t_data_template	data;
+
+	ft_bzero(&data, sizeof(t_data_template));
+	if (cliopts_get(av, g_read_opts, &data))
+		return (1);
+
 	return (0);
 }
